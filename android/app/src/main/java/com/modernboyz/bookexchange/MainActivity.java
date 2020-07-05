187f06e3490b236b415c97841e1edd8c45374384
Apoorv Pandey <apoorvpandey111@gmail.com>
2020-07-05 20:02:18 +0530

    Initial commit

---
+++ android/app/src/main/java/com/modernboyz/bookexchange/MainActivity.java
@@ -0,0 +1,6 @@
+package com.modernboyz.bookexchange;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Homepage"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+                accountName: Text("Apoorv Pandey"),
+                accountEmail: Text("apoorvpandey111@gmail.com"))
+          ],
+        ),
+      ),
+    );
+  }
+}
